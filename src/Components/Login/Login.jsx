b92b740b8d60c4985ec25f4f99bbfd88f6038333
Gamze Güneri <gamzegunerig@gmail.com>
2021-11-17 22:28:41 +0300

    env

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,6 +7,36 @@ import image4 from '../assets/image4.png'
 import styles from './login.module.css'
 
 export default function Login() {
+    const [user, setUser] = useState({
+        email: "",
+        password : ""
+      })
+      const changeData = (id, value) => {
+        const newUser = { ...user, [id]: value }
+        setUser(newUser)
+      }
+    
+  const getUser = async (e) => {
+    e.preventDefault()
+    const result = await fetch(
+      BACKEND_URL + "/users/login" ,
+      {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+
+    if (result.ok) {
+      console.log("Successfully signed in!")
+      console.log(result)
+    } else {
+      console.log("Error with signing in")
+    }
+  }
+
     return (
         <div>
             <Row>
@@ -23,17 +53,12 @@ export default function Login() {
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" id="email" className="form-control" placeholder="Enter email" onChange={(e) => changeData(e.target.id, e.target.value)}/>
                 </div>
 
                 <div className={styles.formGroup}>
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                    </div>
+                    <input type="password" id="password" className="form-control" placeholder="Enter password" onChange={(e) => changeData(e.target.id, e.target.value)}/>
                 </div>
 
                 <button type="submit" className={styles.button}>Submit</button>
